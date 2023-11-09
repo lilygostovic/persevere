6f626e0df6edd0ad93e5fa084ebe2118b3eab971
lilygostovic <lily.gostovic@gmail.com>
2023-11-09 08:03:17 -0500

    Make homepage changes requested from Defne

--- src/components/EventDetails.tsx
+++ src/components/EventDetails.tsx
@@ -1,14 +1,16 @@
-import { useTranslation } from "react-i18next";
+import { useTranslation } from 'react-i18next';
 
-import { StyledDiv, StyledText } from "./common";
+import {
+  StyledDiv,
+  StyledText,
+} from './common';
 
 export const EventDetails = () => {
   const { t } = useTranslation();
   return (
     <StyledDiv display="flex" flexDirection="column" width="33%">
       <StyledText variant="upcoming">{t("upcoming.date")}</StyledText>
-      <StyledText variant="upcoming">{t("upcoming.title")}:</StyledText>
-      <StyledText variant="upcoming">{t("upcoming.subtitle")}</StyledText>
+      <StyledText variant="upcoming">{t("upcoming.title")}</StyledText>
     </StyledDiv>
   );
 };
