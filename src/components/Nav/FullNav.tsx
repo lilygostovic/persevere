f170450d897ebfe12b28755418639a500db2913e
lilygostovic <lily.gostovic@gmail.com>
2023-11-11 12:11:24 -0500

    Add link to googlemaps in Nav

--- src/components/Nav/FullNav.tsx
+++ src/components/Nav/FullNav.tsx
@@ -1,13 +1,11 @@
-import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
+import { useState } from "react";
 
-import { links } from '../../data/Links';
-import { Pages } from '../../types/Pages';
-import {
-  PageButton,
-  StyledDiv,
-  StyledText,
-} from '../common';
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
+
+import { links } from "../../data/Links";
+import { Pages } from "../../types/Pages";
+import { PageButton, StyledDiv, StyledText } from "../common";
 
 const SIDE_WIDTH = "33";
 
@@ -15,32 +13,52 @@ type FullNavProps = {
   active: Pages;
 };
 
-export const FullNav = ({active}: FullNavProps) => {
-    const { t } = useTranslation()
+export const FullNav = ({ active }: FullNavProps) => {
+  const { t } = useTranslation();
 
-    return (
-      <StyledDiv mb="50px">
+  const [isAddressHovered, setIsAddressHovered] = useState(false);
+
+  const toggleHoverAddress = () => {
+    setIsAddressHovered(!isAddressHovered);
+  };
+
+  return (
+    <StyledDiv mb="50px">
+      <StyledDiv
+        display="flex"
+        justifyContent="space-between"
+        mt="20px"
+        mb="40px"
+      >
+        <Link
+          to="/"
+          style={{
+            textDecoration: "none",
+          }}
+        >
+          <StyledText variant="title">{t("title")}</StyledText>
+        </Link>
+
         <StyledDiv
+          width={`${SIDE_WIDTH}%`}
           display="flex"
           justifyContent="space-between"
-          mt="20px"
-          mb="40px"
         >
-          <Link
-            to="/"
+          <a
+            href={t("address.link")}
+            rel="noreferrer"
+            target="_blank"
             style={{
-              textDecoration: "none",
+              color: "black",
+              textDecoration: isAddressHovered ? "underline" : "none",
             }}
           >
-            <StyledText variant="title">{t("title")}</StyledText>
-          </Link>
-
-          <StyledDiv
-            width={`${SIDE_WIDTH}%`}
-            display="flex"
-            justifyContent="space-between"
-          >
-            <StyledDiv display="flex" flexDirection="column">
+            <StyledDiv
+              display="flex"
+              flexDirection="column"
+              onMouseEnter={toggleHoverAddress}
+              onMouseLeave={toggleHoverAddress}
+            >
               <StyledText variant="paragraphMedium">
                 {t("address.line1")}
               </StyledText>
@@ -51,53 +69,54 @@ export const FullNav = ({active}: FullNavProps) => {
                 {t("address.line3")}
               </StyledText>
             </StyledDiv>
-            <StyledDiv>
-              <a
-                href={t("instagram.link")}
-                rel="noreferrer"
-                target="_blank"
-                style={{ textDecoration: "none" }}
+          </a>
+          <StyledDiv>
+            <a
+              href={t("instagram.link")}
+              rel="noreferrer"
+              target="_blank"
+              style={{ textDecoration: "none" }}
+            >
+              <StyledText
+                variant="paragraphMedium"
+                style={{ paddingRight: `15px` }}
               >
-                <StyledText
-                  variant="paragraphMedium"
-                  style={{ paddingRight: `15px` }}
-                >
-                  {t("instagram.label")}
-                </StyledText>
-              </a>
-              <a
-                href={t("email.link")}
-                rel="noreferrer"
-                target="_blank"
-                style={{ textDecoration: "none" }}
-              >
-                <StyledText variant="paragraphMedium">
-                  {t("email.label")}
-                </StyledText>
-              </a>
-            </StyledDiv>
-          </StyledDiv>
-        </StyledDiv>
-        <StyledDiv display="flex" justifyContent="space-between">
-          <StyledDiv display="flex" flexDirection="row">
-            {links.map((link) => (
-              <PageButton
-                key={link.route}
-                displayName={link.displayName}
-                route={link.route}
-                isActive={link.route === active}
-                isFirstItem={link.isFirstItem}
-                isLastItem={link.isLastItem}
-                openInNewWindow={link.openInNewWindow}
-              />
-            ))}
-          </StyledDiv>
-          <StyledDiv width={`${SIDE_WIDTH}%`}>
-            <StyledDiv height="2px" width="100%" bg="white" />
-            <StyledText variant="navItem">{t("upcoming.label")}</StyledText>
-            <StyledDiv height="2px" width="100%" bg="black" />
+                {t("instagram.label")}
+              </StyledText>
+            </a>
+            <a
+              href={t("email.link")}
+              rel="noreferrer"
+              target="_blank"
+              style={{ textDecoration: "none" }}
+            >
+              <StyledText variant="paragraphMedium">
+                {t("email.label")}
+              </StyledText>
+            </a>
           </StyledDiv>
         </StyledDiv>
       </StyledDiv>
-    );
-}
+      <StyledDiv display="flex" justifyContent="space-between">
+        <StyledDiv display="flex" flexDirection="row">
+          {links.map((link) => (
+            <PageButton
+              key={link.route}
+              displayName={link.displayName}
+              route={link.route}
+              isActive={link.route === active}
+              isFirstItem={link.isFirstItem}
+              isLastItem={link.isLastItem}
+              openInNewWindow={link.openInNewWindow}
+            />
+          ))}
+        </StyledDiv>
+        <StyledDiv width={`${SIDE_WIDTH}%`}>
+          <StyledDiv height="2px" width="100%" bg="white" />
+          <StyledText variant="navItem">{t("upcoming.label")}</StyledText>
+          <StyledDiv height="2px" width="100%" bg="black" />
+        </StyledDiv>
+      </StyledDiv>
+    </StyledDiv>
+  );
+};
